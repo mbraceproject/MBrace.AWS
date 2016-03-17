28a97d6570ee877b1ba6805890cd8bc7cf421c92
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2016-03-17 17:13:46 +0200

    add CloudFlow tests

---
+++ tests/MBrace.AWS.Tests/CloudFlowTests.fs
@@ -0,0 +1,43 @@
+﻿namespace MBrace.AWS.Tests.Runtime
+
+open MBrace.Core
+open MBrace.Core.Internals
+open MBrace.Core.Tests
+
+open MBrace.AWS
+open MBrace.AWS.Runtime
+open MBrace.AWS.Tests
+
+open NUnit.Framework
+
+[<AbstractClass; TestFixture>]
+type ``AWS CloudFlow Tests`` (config : Configuration, localWorkers : int) =
+    inherit ``CloudFlow tests`` ()
+    let session = new ClusterSession(config, localWorkers)
+
+    [<TestFixtureSetUp>]
+    member __.Init () = session.Start()
+
+    [<TestFixtureTearDown>]
+    member __.Fini () = session.Stop()
+
+    override __.IsSupportedStorageLevel _ = true
+
+    override __.Run (workflow : Cloud<'T>) = 
+        session.Cluster.Run(workflow)
+
+    override __.RunLocally(workflow : Cloud<'T>) = 
+        session.Cluster.RunLocally(workflow)
+
+    override __.RunWithLogs(workflow : Cloud<unit>) =
+        let cloudProcess = session.Cluster.CreateProcess workflow
+        do cloudProcess.Result
+        cloudProcess.GetLogs () |> Array.map CloudLogEntry.Format
+
+    override __.FsCheckMaxNumberOfTests = 3
+    override __.FsCheckMaxNumberOfIOBoundTests = 3
+
+
+[<Category("Standalone Cluster")>]
+type ``CloudFlow Tests - Standalone Cluster - Remote Storage`` () =
+    inherit ``AWS CloudFlow Tests``(getMBraceConfig None, 4)
