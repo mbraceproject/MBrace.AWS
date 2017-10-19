703331bf62365b89db4bf301f9b3b0d2a22d689c
Eirik Tsarpalis <eirik.tsarpalis@jet.com>
2017-10-19 01:15:45 +0100

    fix path

--- src/MBrace.AWS.StandaloneWorker/AssemblyInfo.fs
+++ src/MBrace.AWS.StandaloneWorker/AssemblyInfo.fs
@@ -5,7 +5,7 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("MBrace.AWS.StandaloneWorker")>]
 [<assembly: AssemblyProductAttribute("MBrace.AWS")>]
 [<assembly: AssemblyDescriptionAttribute("AWS PaaS bindings for MBrace")>]
-[<assembly: AssemblyMetadataAttribute("Release Signature","Version 0.1.10-alpha, Git Hash 2538c8, Build Date 19102017 00:13 +01:00")>]
+[<assembly: AssemblyMetadataAttribute("Release Signature","Version 0.1.10-alpha, Git Hash 847d63, Build Date 19102017 00:14 +01:00")>]
 [<assembly: AssemblyVersionAttribute("0.1.10")>]
 [<assembly: AssemblyFileVersionAttribute("0.1.10")>]
 do ()
@@ -14,6 +14,6 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "MBrace.AWS.StandaloneWorker"
     let [<Literal>] AssemblyProduct = "MBrace.AWS"
     let [<Literal>] AssemblyDescription = "AWS PaaS bindings for MBrace"
-    let [<Literal>] AssemblyMetadata_Release_Signature = "Version 0.1.10-alpha, Git Hash 2538c8, Build Date 19102017 00:13 +01:00"
+    let [<Literal>] AssemblyMetadata_Release_Signature = "Version 0.1.10-alpha, Git Hash 847d63, Build Date 19102017 00:14 +01:00"
     let [<Literal>] AssemblyVersion = "0.1.10"
     let [<Literal>] AssemblyFileVersion = "0.1.10"
